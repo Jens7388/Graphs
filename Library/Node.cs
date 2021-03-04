e62fe90be715d2fa14d568e2491c2c6c21cbcb93
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-03-04 09:42:20 +0100

    Add project files.

---
+++ Library/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Library
+{
+    public class Node<T>
+    {
+        protected T _item;
+
+        public Node()
+        {
+
+        }
+
+        public Node(T item)
+        {
+            _item = item;
+        }
+
+        public virtual T Item
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Item.ToString();
+        }
+    }
+}
---
+++ Library/Vertex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Vertex<T>: Node<T>
+    {
+        protected bool visited;
+        protected List<Vertex<T>> neighbors;
+
+        public Vertex(T item){
+            _item = item;
+        }
+
+        public virtual void AddNeighbor(Vertex<T> neighbor)
+        {
+            neighbors.Add(neighbor);
+        }
+
+        public virtual bool HasNeighbor(Vertex<T> neighbor)
+        {
+            if(neighbors.Contains(neighbor))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
